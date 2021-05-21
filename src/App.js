b0aaf229b4a555e33248276c7c898302d6080df8
marinabir94 <marinabir94@gmail.com>
2021-05-21 17:46:36 +0200

    Added astronomic fake data and moved searchbar to right

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
         </form>
         </div>
         <section className="locationInfo">
-          Paris
-          France
-          2021-05-20 19:06
+          <div className="name">Paris, France</div>
+          <div className="country">France</div>
+          <div className="datetime">2021-05-20 19:06</div>
         </section>
         <section className="weather-current">
 
@@ -166,7 +166,27 @@ function App() {
           </table>
         </section>
         <section  className="astronomic">
-          Astronomic data
+        <table className="astronomic-table">
+            <tr>
+              <th>Sunrise</th>
+              <th>Sunset</th>
+              <th>Moonrise</th>
+              <th>Moonset</th>
+            </tr>
+            <tr>
+              <td><img src="https://img.icons8.com/carbon-copy/100/000000/sunrise.png" alt="Sunrise" title="Sunrise"/></td>
+              <td><img src="https://img.icons8.com/carbon-copy/100/000000/sunset--v1.png" alt="Sunset" title="Sunset"/></td>
+              <td><img src="https://img.icons8.com/dotty/80/000000/moonrise.png" alt="Moonrise" title="Moonrise"/></td>
+              <td><img src="https://img.icons8.com/dotty/80/000000/moonset.png" alt="Moonset" title="Moonset"/></td>
+            </tr>
+            <tr>
+              <td>06:43 AM</td>
+              <td>09:10 PM</td>
+              <td>02:12 PM</td>
+              <td>03:24 AM</td>
+            </tr>
+          
+          </table>
         </section>
       </main>
       <footer>
