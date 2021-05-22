8aeeb668810e8f983329a991f970bd1a1fa9d117
marinabir94 <marinabir94@gmail.com>
2021-05-22 20:20:00 +0200

    Adjusted some minor sizes in the asronomic section

--- src/App.js
+++ src/App.js
@@ -174,10 +174,10 @@ function App() {
               <th>Moonset</th>
             </tr>
             <tr>
-              <td><img src="https://img.icons8.com/carbon-copy/100/000000/sunrise.png" alt="Sunrise" title="Sunrise"/></td>
-              <td><img src="https://img.icons8.com/carbon-copy/100/000000/sunset--v1.png" alt="Sunset" title="Sunset"/></td>
-              <td><img src="https://img.icons8.com/dotty/80/000000/moonrise.png" alt="Moonrise" title="Moonrise"/></td>
-              <td><img src="https://img.icons8.com/dotty/80/000000/moonset.png" alt="Moonset" title="Moonset"/></td>
+              <td><img className="sun" src="https://img.icons8.com/carbon-copy/100/000000/sunrise.png" alt="Sunrise" title="Sunrise"/></td>
+              <td><img className="sun" src="https://img.icons8.com/carbon-copy/100/000000/sunset--v1.png" alt="Sunset" title="Sunset"/></td>
+              <td><img className="moon" src="https://img.icons8.com/dotty/80/000000/moonrise.png" alt="Moonrise" title="Moonrise"/></td>
+              <td><img className="moon" src="https://img.icons8.com/dotty/80/000000/moonset.png" alt="Moonset" title="Moonset"/></td>
             </tr>
             <tr>
               <td>06:43 AM</td>
